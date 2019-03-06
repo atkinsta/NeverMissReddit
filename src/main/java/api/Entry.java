e33abc81c910520aa509ba47e84e749ae512d923
atkinsta <atkinstaa@gmail.com>
2019-03-05 20:44:38 -0500

    Initial commit to Github

---
+++ src/main/java/api/Entry.java
@@ -0,0 +1,11 @@
+package api;
+
+import com.mashape.unirest.http.Unirest;
+import org.apache.http.HttpHost;
+
+public class Entry {
+    public static void main (String[] args) {
+
+    }
+}
+
---
+++ src/main/java/api/RedditAPI.java
@@ -0,0 +1,5 @@
+package api;
+
+public class RedditAPI {
+    final String url =
+}
