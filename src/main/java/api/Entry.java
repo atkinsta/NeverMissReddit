30bd1c66c77f7bdf34970d63a194628ae9d02c8a
atkinsta <atkinstaa@gmail.com>
2019-03-12 21:26:10 -0400

    Getting the auth up and running

--- src/main/java/api/Entry.java
+++ src/main/java/api/Entry.java
@@ -1,11 +1,20 @@
 package api;
 
-import com.mashape.unirest.http.Unirest;
-import org.apache.http.HttpHost;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import java.io.IOException;
 
 public class Entry {
-    public static void main (String[] args) {
+    public static void main (String[] args) throws IOException {
+        String token = null;
+        OAuth auth = new OAuth();
+        try {
+            token = auth.getToken();
+        } catch (UnirestException err) {
+            err.printStackTrace();
+        }
 
+        System.out.println(token);
     }
 }
 
--- src/main/java/api/OAuth.java
+++ src/main/java/api/OAuth.java
@@ -1,8 +1,55 @@
 package api;
 
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.apache.http.client.HttpClient;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
 public class OAuth {
-    private static final String REDDIT_APP_ID = System.getProperty("REDDIT_APP_ID");
-    private static final String REDDIT_SECRET = System.getProperty("REDDIT_SECRET");
+    private final String GET_TOKEN_URL = "https://www.reddit.com/api/v1/access_token";
+    private String REDDIT_APP_ID;
+    private String REDDIT_SECRET;
+    private String REDDIT_USER;
+    private String REDDIT_PASS;
+    private Properties props = new Properties();
+    private FileInputStream config;
 
+    public String getToken() throws UnirestException, IOException {
 
+        //Get path to config file at runtime.
+        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath() + "config.properties";
+        config = new FileInputStream(rootPath);
+        props.load(config);
+
+        setVars();
+
+        String body = String.format("{\"grant_type\": \"password\", \"username\": \"%s\", \"password\": \"%s\"}", REDDIT_USER, REDDIT_PASS);
+
+        Unirest.clearDefaultHeaders();
+
+        HttpResponse<JsonNode> response = Unirest.post(GET_TOKEN_URL).basicAuth(REDDIT_APP_ID, REDDIT_SECRET)
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .header("User-Agent", "atkinty:atkinty.NeverMissReddit/0.2 (by u/R4nd0mnumbrz)")
+                .body(body)
+                .asJson();
+
+        return response.getBody().toString();
+    };
+
+    private void setVars() {
+        REDDIT_APP_ID = props.getProperty("REDDIT_APP_ID");
+        REDDIT_SECRET = props.getProperty("REDDIT_SECRET");
+        REDDIT_USER = props.getProperty("REDDIT_USER");
+        REDDIT_PASS = props.getProperty("REDDIT_PASS");
+    }
 }
